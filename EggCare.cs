25f2514e37e6a4f25140e386934627e7b56cd55b
Kamil <mentos.96@o2.pl>
2023-05-28 21:25:46 +0200

    Write subclass EggCare

--- EggCare.cs
+++ EggCare.cs
@@ -9,14 +9,16 @@ namespace BeehiveManagmentSystem
     internal class EggCare : Bee
 
     {
+        public const float CARE_PROGRESS_PER_SHIFT = 0.15f;
 
         protected new float CostPerShift { get; } = 1.35f;
-
-        public EggCare() :base("EggCare") { }
+        private Queen queen;
+        public EggCare(Queen queen) :base("EggCare") {this.queen = queen; }
+        //public EggCare() :base("EggCare") { }
 
         protected override void DoJob()
         {
-            /*przesłaniana w podklasie*/
+            queen.CareOfEggs(CARE_PROGRESS_PER_SHIFT);
         }
     }
 }
