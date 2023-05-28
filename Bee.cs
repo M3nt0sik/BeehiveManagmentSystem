efbc6afd5ea08796eeaab534b257a0d5cfc0005c
Kamil <mentos.96@o2.pl>
2023-05-28 19:48:58 +0200

    Writae base Clase Bee

---
+++ Bee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagmentSystem
+{
+    internal class Bee
+    {
+        private string job; 
+        protected float CostPerShift { get; }
+
+        public Bee(string job)
+        {
+            this.job = job;
+        }
+
+        public void TheNextShift()
+        {
+           if(HoneyVault.ConsumeHoney(CostPerShift))
+            {
+                DoJob();
+            }
+
+        }
+
+        protected virtual void DoJob()
+        {
+            /*przesłaniana w podklasie*/
+        }
+
+    }
+}
